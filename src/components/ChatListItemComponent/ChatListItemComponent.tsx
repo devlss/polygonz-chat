a70fb825d6bfd9553222465841a0ed3c08cd4450
devlss <1965949+devlss@users.noreply.github.com>
2022-04-18 23:59:22 +0300

    style: Поправлено отображения счетчика сообщений

--- src/components/ChatListItemComponent/ChatListItemComponent.tsx
+++ src/components/ChatListItemComponent/ChatListItemComponent.tsx
@@ -1,21 +1,19 @@
 import {FC} from 'react';
+import {LastMessageComponent} from '../LastMessageComponent';
+import {LogoComponent} from '../LogoComponent';
 import type {ChatListItemComponentProps} from './ChatListItemComponent.types';
 
 import './ChatListItemComponent.scss';
-import {LastMessageComponent} from '../LastMessageComponent';
-import {LogoComponent} from '../LogoComponent';
 
 export const ChatListItemComponent: FC<ChatListItemComponentProps> = ({chat, className}) => {
 	return (
 		<div id={chat.id.toString()} className={`chat-list-item ${className || ''}`}>
-			<div className="chat-list-item__logo noevent"><LogoComponent src={chat.avatar} title={chat.title}></LogoComponent></div>
+			<div className="chat-list-item__logo noevent">
+				<LogoComponent src={chat.avatar} title={chat.title}></LogoComponent>
+			</div>
 			<div className="chat-list-item__title noevent">
 				<span className="chat-list-item__title-text">{chat.title}</span>
-				{!!chat.unread_count && (
-					<span className="chat-list-item__count">
-						<span className="chat-list-item__count-text">{chat.unread_count}</span>
-					</span>
-				)}
+				{!!chat.unread_count && <span className="chat-list-item__count">{chat.unread_count}</span>}
 			</div>
 			<div className="chat-list-item__message noevent">{chat.last_message && <LastMessageComponent lastMessage={chat.last_message} />}</div>
 		</div>
